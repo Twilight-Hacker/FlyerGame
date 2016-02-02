923204cfbb99f4b97cc63490f1318356b0d699f3
Fragkas Vasileios <swilliammccoy@gmail.com>
2016-02-02 16:33:02 +0200

    Creating animated background

--- app/src/main/java/com/galadar/flyergame/GameScreen.java
+++ app/src/main/java/com/galadar/flyergame/GameScreen.java
@@ -1,14 +1,72 @@
 package com.galadar.flyergame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+public class GameScreen extends SurfaceView {
 
-public class GameScreen extends SurfaceView{
+    private Bitmap bmp;
+    private SurfaceHolder holder;
+
     public GameScreen(Context context) {
         super(context);
 
+        this.setDrawingCacheEnabled(true);
 
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.testbg);
+
+        holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+            }
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas c = holder.lockCanvas(null);
+                onMyDraw(c);
+                holder.unlockCanvasAndPost(c);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+        });
     }
 
+    public void bganimation(int time){
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bgbasis);
+        Canvas canvas = getHolder().lockCanvas();
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        BitmapShader mShader1 = new BitmapShader(bmp, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        paint.setShader(mShader1);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bgfull);
+        if(canvas==null) Log.e("SysError", "Canvas Null");
+        canvas.drawBitmap(bitmap, -time, 0, paint);
+        getHolder().unlockCanvasAndPost(canvas);
+    }
+
+    protected void onMyDraw(Canvas canvas) {
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bgbasis);
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        BitmapShader mShader1 = new BitmapShader(bmp, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        paint.setShader(mShader1);
+        canvas.drawPaint(paint); // .drawBitmap(bmp, 10, 10, paint);
+    }
+
+    public Bitmap get(){
+        return this.getDrawingCache();
+    }
+
+
+
 }
+
--- app/src/main/java/com/galadar/flyergame/MainActivity.java
+++ app/src/main/java/com/galadar/flyergame/MainActivity.java
@@ -2,31 +2,35 @@ package com.galadar.flyergame;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity{
 
+    final static int sdkVer = Build.VERSION.SDK_INT;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-/*
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int height = displaymetrics.heightPixels;
-        int width = displaymetrics.widthPixels;
-*/
-
 
         RelativeLayout main = (RelativeLayout)findViewById(R.id.ScreenView);
 
-        final ImageView character = new ImageView(this);
+        final GameScreen background = new GameScreen(this);
+        RelativeLayout.LayoutParams bgparams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        main.addView(background, bgparams);
 
+
+        final ImageView character = new ImageView(this);
         character.setMaxHeight(35);
         character.setMaxWidth(30);
         character.setBackgroundColor(Color.BLUE);
@@ -40,13 +44,25 @@ public class MainActivity extends Activity{
         main.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                character.setX(event.getX()- (character.getWidth()/2) );
-                character.setY(event.getY()- (character.getHeight()/2) );
+                character.setX(event.getX() - (character.getWidth() / 2));
+                character.setY(event.getY() - (character.getHeight() / 2));
                 character.invalidate();
                 return true;
             }
         });
 
+        //Handler to forward background
+        final Handler BgHandler = new Handler(Looper.getMainLooper());
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                background.bganimation(5);
+                BgHandler.postDelayed(this, 500);
+            }
+        };
+
+        r.run();
 
     }
 }
